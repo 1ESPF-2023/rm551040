a04c89dbdad8d6297e6caea58c5bd4e1c6ab621e
lucasvital22 <rm551040@fiap.com.br>
2023-05-22 09:03:47 -0300

    Criação de obj + el msg no login/ val form

--- js/login.js
+++ js/login.js
@@ -1,9 +1,6 @@
-let usuario = "";
-let senha = "";
+"use strick";
 
-const botaoSubmit = document.querySelector("#btnSubmit");
-
-botaoSubmit.addEventListener("click",()=>{
+addEventListener("click", (evt)=>{
 
     const inputUser = document.querySelector("input[name='nmUser']");
     const inputPass = document.querySelector("input[name='pass']");
@@ -11,11 +8,36 @@ botaoSubmit.addEventListener("click",()=>{
     const labelUser = document.querySelector("label[for='idUser']");
     const labelPass = document.querySelector("label[for='idPass']");
 
-    if(inputUser.value.length > 3 && inputPass.value.length > 3){
-        // inputUser.setAttribute("style", "outline-color:#00ff00;")
-        // inputPass.setAttribute("style", "outline-color:#00ff00;")
-        labelUser.setAttribute("style","color:#00ff00;");
-        labelPass.setAttribute("style","color:#00ff00;");
-    }
+//recuerar o cnteudo ds camos e adicionar em um objeto
+//CRIANDO O OBJETO USUARIO-LOGADO
+console.log('aaaaaaa')
+const usuarioLogado = {
+    nomeUsuarioLogado : inputUser.value,
+    senhaUsuarioLogado : inputPass.value,
+}
 
-});
+    if(evt.target.if =="btnSubmit" ){}
+        // console.log("ACHEI O BOTAO")
+    
+        if(inputUser.value.length > 3 && inputPass.value.length > 3){
+            // inputUser.setAttribute("style", "outline-color:#00ff00;")
+            // inputPass.setAttribute("style", "outline-color:#00ff00;")
+            labelUser.setAttribute("style","color:#00ff00;");
+            labelPass.setAttribute("style","color:#00ff00;");
+
+            //se preenheu a qtd de caracteries necesaria 
+            //agora vamos valdiar o nomeUsuario e senhaUsuaio
+            if(usuarioLogado.nomeUsuarioLogado == "tutu" && usuarioLogado.senhaUsuarioLogado =="123456"){
+                alert("VALIDADO");
+            }else(
+                alert("NAO VALIDADO")
+            )
+
+
+
+        }else{
+            labelUser.setAttribute("style","color:#ff0000;");
+            labelPass.setAttribute("style","color:#ff0000;");
+        }
+    
+})
